ea77895396df70995eee95a4a76e2d9ce3bfca71
TaylorArmstrong <TArmstrong1327@gmail.com>
2018-12-01 12:59:50 -0700

    taking a break, working on compose message

--- src/components/compose/compose.js
+++ src/components/compose/compose.js
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default class Compose extends React.Component {
 
+    // constructor(props) {
+    //     super(props)
+    // }
+
+    // state = {
+    //     subject: '',
+    //     body: ''
+    // }
+
+    // onChange = () => {
+    //     this.setState(
+    //         {
+    //             subject: document.forms.composeMessageForm.subject.value,
+    //             body: document.forms.composeMessageForm.body.value
+    //         }
+    //     )
+    // }
+
     composeMessage = (ev) => {
         ev.preventDefault()
         // console.log('ev', ev)
@@ -24,31 +42,42 @@ export default class Compose extends React.Component {
     }
 
     render() {
+        // const { subject, body } = this.state
+        // const { isComposing } = this.props
+
+        // if (!isComposing) {
+        //     return (
+        //         <span />
+        //     )
+        // }
+
         return (
-            <form className="form-horizontal well" onSubmit={this.composeMessage}>
-                <div className="form-group">
-                    <div className="col-sm-8 col-sm-offset-2">
-                        <h4>Compose Message</h4>
+            <div>
+                <form id='composeMessageForm' className="form-horizontal well" onSubmit={this.composeMessage}>
+                    <div className="form-group">
+                        <div className="col-sm-8 col-sm-offset-2">
+                            <h4>Compose Message</h4>
+                        </div>
                     </div>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="subject" className="col-sm-2 control-label">Subject</label>
-                    <div className="col-sm-8">
-                        <input type="text" className="form-control" id="subject" placeholder="Enter a subject" name="subject" />
+                    <div className="form-group">
+                        <label htmlFor="subject" className="col-sm-2 control-label">Subject</label>
+                        <div className="col-sm-8">
+                            <input type="text" className="form-control" id="subject" placeholder="Enter a subject" name="subject" />
+                        </div>
                     </div>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="body" className="col-sm-2 control-label">Body</label>
-                    <div className="col-sm-8">
-                        <textarea name="body" id="body" className="form-control" />
+                    <div className="form-group">
+                        <label htmlFor="body" className="col-sm-2 control-label">Body</label>
+                        <div className="col-sm-8">
+                            <textarea name="body" id="body" className="form-control" />
+                        </div>
                     </div>
-                </div>
-                <div className="form-group">
-                    <div className="col-sm-8 col-sm-offset-2">
-                        <input type="submit" value="Send" className="btn btn-primary" />
+                    <div className="form-group">
+                        <div className="col-sm-8 col-sm-offset-2">
+                            <input type="submit" value="Send" className="btn btn-primary" />
+                        </div>
                     </div>
-                </div>
-            </form>
+                </form>
+            </div>
         )
     }
 
